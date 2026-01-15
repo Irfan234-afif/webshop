2b93db79b774760e20e0364954da5c0379c348a3
irfan <apipiirpan@gmail.com>
2026-01-15 23:55:30 +0700

    fix: Set default website warehouse only when creating a new item.

--- webshop/webshop/doctype/website_item/website_item.js
+++ webshop/webshop/doctype/website_item/website_item.js
@@ -5,10 +5,12 @@ frappe.ui.form.on('Website Item', {
 	onload: (frm) => {
 		// should never check Private
 		frm.fields_dict["website_image"].df.is_private = 0;
-		frappe.db.get_single_value('Webshop Settings', 'default_warehouse')
+		if (frm.is_new()) {
+			frappe.db.get_single_value('Webshop Settings', 'default_warehouse')
 			.then(default_warehouse => {
 				frm.set_value('website_warehouse', default_warehouse);
 			})
+		}
 	},
 
 	refresh: (frm) => {
