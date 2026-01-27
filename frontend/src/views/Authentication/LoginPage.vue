8cf704599aa777bcffaba8d5da7260dcc9fb8c46
irfan <apipiirpan@gmail.com>
2026-01-28 00:02:45 +0700

    handle enter

--- frontend/src/views/Authentication/LoginPage.vue
+++ frontend/src/views/Authentication/LoginPage.vue
@@ -47,6 +47,7 @@
                 required
                 class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent outline-none transition pr-12"
                 placeholder="••••••••"
+                @keyup.enter="handleLogin"
               />
               <button
                 type="button"
